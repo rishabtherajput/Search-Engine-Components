4606ba147788536370eae963142d777df6c055a0
rishabtherajput <44002814+rishabtherajput@users.noreply.github.com>
2018-11-04 15:07:53 +0530

    Add files via upload

--- webcrawler.py
+++ webcrawler.py
@@ -3,23 +3,27 @@ from bs4 import BeautifulSoup
 
 def trade(max_pages):
     page = 1
-    f = open('web.txt', 'w')
     while page <= max_pages:
         url = 'https://nicoblog.org/pc-games/page/'+ str(page)
         source_code = requests.get(url)
         plain_text =  source_code.text
         soup = BeautifulSoup(plain_text,features="html.parser")
-        for link in soup.find_all('a'):
+        for link in soup.find_all('a',{'rel':'bookmark'}):
             href = link.get('href')
             title = link.string
-            print(href)
-            print(title)
-            f.write(href+'\n')
-            f.write(str(title))
+            #print(href)
+            #print(title)
+            get_single(href)
         page = page+1
 
-    f.close()
-trade(4)
+
+def get_single(item_url):
+    source_code = requests.get(item_url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text, features="html.parser")
+    for item_name in soup.find_all('p'):
+        print(item_name.string)
 
 
 
+trade(1)
